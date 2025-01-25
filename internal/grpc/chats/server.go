120c4f4b91a68c6d186b1180a85ee3fad9c48f91
Roman <r.yakushin.1070@mail.ru>
2025-01-25 13:58:11 +0300

    chats WIP

--- internal/grpc/chats/server.go
+++ internal/grpc/chats/server.go
@@ -2,8 +2,10 @@ package chats
 
 import (
 	"context"
+	"fmt"
 	chatsv1 "idea-store-auth/gen/go/chats"
 	"idea-store-auth/internal/domain/models"
+	"log/slog"
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -11,10 +13,10 @@ import (
 
 type Chats interface {
 	SendMessage(ctx context.Context, senderId, recieverId int64, fileName, text, creationDate string) (*emptypb.Empty, error)
-	GetMessages(ctx context.Context, senderId, recieverId int64) (*[]models.Message, error)
+	GetMessages(ctx context.Context, senderId, recieverId int64) ([]*models.Message, error)
 	CreateChat(ctx context.Context, user1, user2 models.ChatUserData) (*emptypb.Empty, error)
-	GetUsersChats(ctx context.Context, userId int64) (*[]models.ChatData, error)
-	DeleteChar(ctx context.Context, chatId int64) (*emptypb.Empty, error)
+	GetUsersChats(ctx context.Context, userId int64) ([]*models.ChatData, error)
+	DeleteChat(ctx context.Context, chatId int64) (*emptypb.Empty, error)
 }
 
 type serverAPI struct {
@@ -24,4 +26,99 @@ type serverAPI struct {
 
 func Register(gRPC *grpc.Server, chats Chats) {
 	chatsv1.RegisterChatsServer(gRPC, &serverAPI{chats: chats})
+}
+
+func (s *serverAPI) SendMessage(ctx context.Context, req *chatsv1.SendMessageRequest) (*emptypb.Empty, error) {
+	slog.Info("grpc started SendMessage")
+
+	_, err := s.chats.SendMessage(ctx, req.Data.SenderId, req.Data.RecieverId, req.Data.FileName, req.Data.Text, req.Data.SendingDate)
+	if err != nil {
+		slog.Error("grpc error SendMessage")
+		return nil, fmt.Errorf("grpc SendMessage error :%v", err.Error())
+	}
+	return nil, nil
+}
+func (s *serverAPI) GetMessages(ctx context.Context, req *chatsv1.GetMessagesRequest) (*chatsv1.GetMessagesResponse, error) {
+	slog.Info("grpc started GetMessages")
+
+	resp, err := s.chats.GetMessages(ctx, req.SenderId, req.RecieverId)
+
+	if err != nil {
+		slog.Error("grpc error GetMessages")
+		return nil, fmt.Errorf("grpc GetMessages error :%v", err.Error())
+	}
+	var messages []*chatsv1.MessageData
+	for _, m := range resp {
+		messages = append(messages, &chatsv1.MessageData{
+			SenderId:    m.SenderId,
+			RecieverId:  m.RecieverId,
+			FileName:    m.Filename,
+			Text:        m.Text,
+			SendingDate: m.Text,
+		})
+	}
+	return &chatsv1.GetMessagesResponse{
+		Messages: messages,
+	}, nil
+}
+
+func (s *serverAPI) CreateChat(ctx context.Context, req *chatsv1.CreateChatRequest) (*emptypb.Empty, error) {
+	slog.Info("grpc satarted CreateChat")
+	firstData := models.ChatUserData{
+		UserId:   req.FirstData.Id,
+		Username: req.FirstData.Name,
+		Avatar:   req.FirstData.Avatar,
+	}
+	secondData := models.ChatUserData{
+		UserId:   req.SecondData.Id,
+		Username: req.SecondData.Name,
+		Avatar:   req.SecondData.Avatar,
+	}
+	_, err := s.chats.CreateChat(ctx, firstData, secondData)
+
+	if err != nil {
+		slog.Error("grpc error CreateChat")
+		return nil, fmt.Errorf("grpc CreateChat error :%v", err.Error())
+	}
+	return nil, nil
+}
+
+func (s *serverAPI) GetUsersChats(ctx context.Context, req *chatsv1.GetUsersChatsRequest) (*chatsv1.GetUsersChatsResponse, error) {
+	slog.Info("grpc started GetUsersChats")
+
+	resp, err := s.chats.GetUsersChats(ctx, req.UserId)
+	if err != nil {
+		slog.Error("grpc error GetUsersChats")
+		return nil, fmt.Errorf("grpc GetUsersChats error :%v", err.Error())
+	}
+	var chats []*chatsv1.ChatData
+	for _, chat := range resp {
+		if chat.User1.UserId == req.UserId {
+			chats = append(chats, &chatsv1.ChatData{
+				Id:     chat.User2.UserId,
+				Name:   chat.User2.Username,
+				Avatar: chat.User2.Avatar,
+			})
+		} else {
+			chats = append(chats, &chatsv1.ChatData{
+				Id:     chat.User1.UserId,
+				Name:   chat.User1.Username,
+				Avatar: chat.User1.Avatar,
+			})
+		}
+	}
+	return &chatsv1.GetUsersChatsResponse{
+		Chats: chats,
+	}, nil
+}
+
+func (s *serverAPI) DeleteChat(ctx context.Context, req *chatsv1.DeleteChatRequest) (*emptypb.Empty, error) {
+	slog.Info("grpc started to DeleteChat")
+
+	_, err := s.chats.DeleteChat(ctx, req.ChatId)
+	if err != nil {
+		slog.Error("grpc error DeleteChat")
+		return nil, fmt.Errorf("grpc DeleteChat error :%v", err.Error())
+	}
+	return nil, nil
 }
---
+++ internal/services/chats/chats.go
@@ -0,0 +1,85 @@
+package chats
+
+import (
+	"context"
+	"fmt"
+	"idea-store-auth/internal/domain/models"
+	"idea-store-auth/internal/grpc/chats"
+	"log/slog"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Chats struct {
+	log *slog.Logger
+	Api chats.Chats
+}
+
+func New(log *slog.Logger, api chats.Chats) *Chats {
+	return &Chats{
+		log: log,
+		Api: api,
+	}
+}
+func (c *Chats) SendMessage(ctx context.Context, message models.Message) (*emptypb.Empty, error) {
+	c.log.Info("service started SendMessage")
+
+	_, err := c.Api.SendMessage(ctx, message.SenderId, message.RecieverId, message.Filename, message.Text, message.CreationDate)
+
+	if err != nil {
+		c.log.Error("service error SendMessage: " + err.Error())
+		return nil, fmt.Errorf("serivce error SendMessage: %v", err.Error())
+	}
+
+	return nil, nil
+}
+
+func (c *Chats) GetMessages(ctx context.Context, senderId, recieverId int64) ([]*models.Message, error) {
+	c.log.Info("service started GetMessages")
+
+	resp, err := c.Api.GetMessages(ctx, senderId, recieverId)
+
+	if err != nil {
+		c.log.Error("service error GetMessages: " + err.Error())
+		return nil, fmt.Errorf("serivce error GetMessages: %v", err.Error())
+	}
+
+	return resp, nil
+}
+
+func (c *Chats) CreateChat(ctx context.Context, user1, user2 models.ChatUserData) (*emptypb.Empty, error) {
+	c.log.Info("service started CreateChat")
+	_, err := c.Api.CreateChat(ctx, user1, user2)
+
+	if err != nil {
+		c.log.Error("service error CreateChat: " + err.Error())
+		return nil, fmt.Errorf("serivce error CreateChat: %v", err.Error())
+	}
+
+	return nil, nil
+}
+
+func (c *Chats) GetUsersChats(ctx context.Context, userId int64) ([]*models.ChatData, error) {
+	c.log.Info("service started GetUsersChats")
+
+	resp, err := c.Api.GetUsersChats(ctx, userId)
+
+	if err != nil {
+		c.log.Error("service error GetUsersChats: " + err.Error())
+		return nil, fmt.Errorf("serivce error GetUsersChats: %v", err.Error())
+	}
+
+	return resp, nil
+}
+
+func (c *Chats) DeleteChat(ctx context.Context, chatId int64) (*emptypb.Empty, error) {
+	c.log.Info("service started DeleteChat")
+	_, err := c.Api.DeleteChat(ctx, chatId)
+
+	if err != nil {
+		c.log.Error("service error DeleteChat: " + err.Error())
+		return nil, fmt.Errorf("serivce error DeleteChat: %v", err.Error())
+	}
+
+	return nil, nil
+}
